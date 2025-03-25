aa999bfab32b365b1dd9f4d478d77544174d707c
Mahanthi Raju <mahanthi0505@gmail.com>
2025-03-24 20:24:06 -0400

    assignment

--- OddEven.py
+++ OddEven.py
@@ -1,6 +1,6 @@
 #Write a 1 to 10 even number? Write a 1 to 10 odd number?
 for n in range(0,11):
     if n % 2==0:
-        print("Even")
+        print("Even",n)
     else:
-        print("odd")
+        print("odd",n)
